81cbf9700877a8d3c7e5fc7ce3ff9039c7d029c0
Isuru Bandara <isurubandara318@gmail.com>
2026-01-03 19:41:11 +0530

    using bun

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -151,7 +151,7 @@ export default function RegisterPage() {
                 )}
               </div>
             </CardContent>
-            <CardFooter className="flex flex-col space-y-4">
+            <CardFooter className="flex flex-col space-y-4 mt-4">
               <Button type="submit" className="w-full" disabled={isLoading}>
                 {isLoading ? (
                   <>
